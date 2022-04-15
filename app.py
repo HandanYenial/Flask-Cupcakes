684a8f76014b35c2557391eb65e598533bdd2336
Handan Yenial <88174651+HandanYenial@users.noreply.github.com>
2022-04-15 00:24:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Cupcake
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "oh-so-secret" 
+app.config['SECRET_KEY'] = "notsecretsecretkey" 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes_db' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
